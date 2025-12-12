0d37796c4e45c1d170cc8157cb3e4dff49bc4cf7
zstg <o0vckutt@duck.com>
2025-12-12 20:48:00 +0530

    Changes

--- flake.nix
+++ flake.nix
@@ -1,9 +1,25 @@
 {
   description = "StratOS Fish configuration";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }: {
-    homeManagerModules.default = import ./module.nix self;
+    nixosModules.default = { lib, pkgs, config, ... }: {
+      programs.fish = {
+        enable = true;
+        vendor.config.enable = true;
+        
+        # Symlink repo's .config/fish structure directly
+        configDir = lib.cleanSourceWith {
+          src = self + "/.config/fish";
+          filter = path: type:
+            lib.cleanSourceFilter path type &&
+            (builtins.baseNameOf path != "README.md") &&
+            (builtins.baseNameOf path != "flake.nix") &&
+            (builtins.baseNameOf path != "flake.lock") &&
+            (builtins.match ".*\.fish$" path != null || builtins.baseNameOf path == "conf.d" || builtins.baseNameOf path == "functions");
+        };
+      };
+    };
   };
 }
